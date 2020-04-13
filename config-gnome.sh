b4d42e641ab7e2a467de28f7e807e6f0fbcb61b0
Khoa Dien <khoa.dien@hotmail.com>
2020-04-13 10:56:39 -0300

    Initial commit

---
+++ config-gnome.sh
@@ -0,0 +1,46 @@
+# GNOME Desktop settings
+gsettings set org.gnome.desktop.datetime automatic-timezone true
+gsettings set org.gnome.desktop.interface clock-format '12h'
+gsettings set org.gnome.desktop.interface clock-show-weekday true
+gsettings set org.gnome.desktop.interface gtk-enable-primary-paste false
+gsettings set org.gnome.desktop.interface gtk-im-module 'gtk-im-context-simple'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
+gsettings set org.gnome.desktop.interface monospace-font-name 'Monospace 11'
+gsettings set org.gnome.desktop.interface show-battery-percentage true
+gsettings set org.gnome.desktop.media-handling autorun-never true
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false
+gsettings set org.gnome.desktop.peripherals.touchpad tap-and-drag false
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true
+gsettings set org.gnome.desktop.peripherals.touchpad two-finger-scrolling-enabled true
+gsettings set org.gnome.desktop.privacy remove-old-temp-files true
+gsettings set org.gnome.desktop.privacy remove-old-trash-files true
+gsettings set org.gnome.desktop.sound input-feedback-sounds false
+gsettings set org.gnome.desktop.wm.keybindings close ['<Super>q']
+gsettings set org.gnome.desktop.wm.keybindings show-desktop ['<Super>d']
+gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier '<Alt>'
+gsettings set org.gnome.desktop.wm.preferences resize-with-right-button true
+gsettings set org.gnome.mutter attach-modal-dialogs false
+gsettings set org.gnome.mutter center-new-windows true
+gsettings set org.gnome.settings-daemon.plugins.media-keys area-screenshot-clip ['<Shift><Super>s']
+gsettings set org.gnome.settings-daemon.plugins.media-keys control-center ['<Super>i']
+gsettings set org.gnome.settings-daemon.plugins.media-keys home ['<Super>e']
+gsettings set org.gnome.settings-daemon.plugins.power power-button-action 'suspend'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-type 'nothing'
+gsettings set org.gnome.settings-daemon.plugins.xsettings antialiasing 'rgba'
+gsettings set org.gnome.settings-daemon.plugins.xsettings hinting 'slight'
+gsettings set org.gnome.shell.keybindings toggle-message-tray ['<Super>m']
+
+
+# GNOME Applications settings
+sudo sed -i "s/^Exec=gnome-terminal$/Exec=gnome-terminal --window --maximize/" /usr/share/applications/org.gnome.Terminal.desktop
+
+gsettings set org.gnome.gedit.preferences.editor auto-indent true
+gsettings set org.gnome.gedit.preferences.editor bracket-matching true
+gsettings set org.gnome.gedit.preferences.editor display-line-numbers true
+gsettings set org.gnome.gedit.preferences.editor display-right-margin false
+gsettings set org.gnome.gedit.preferences.editor highlight-current-line true
+gsettings set org.gnome.gedit.preferences.editor insert-spaces true
+gsettings set org.gnome.gedit.preferences.editor scheme 'oblivion'
+gsettings set org.gnome.gedit.preferences.editor tabs-size 4
+gsettings set org.gnome.gedit.preferences.editor wrap-last-split-mode 'word'
---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+
+# Configure GNOME settings
+./config-gnome.sh
+
+# Enable additional repos
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf copr enable dawid/better_fonts -y
+sudo dnf copr enable pschyska/alacritty -y
+dnf check-update
+
+# Remove bloat
+sudo dnf remove $(cat ./pkg.remove) -y
+
+# Install packages from repo
+sudo dnf install $(cat ./pkg.add) -y
+
+# Install PureLine Bash Prompt
+mkdir -p ~/pureline
+git clone https://github.com/chris-marsh/pureline.git ~/pureline
+cp ~/pureline/configs/powerline_full_256col.conf ~/.pureline.conf
+echo -e ". ~/pureline/pureline ~/.pureline.conf\n" >> ~/.bashrc
+
+# Extract Meslo fonts
+mkdir -p ~/.fonts/meslo-nf
+tar -xzvf meslo-nf.tar.gz -C ~/.fonts/meslo-nf
+
+# Create custom font config
+mkdir -p ~/.config/fontconfig
+cp fonts.conf ~/.config/fontconfig
+
+# Install Google Chrome
+sudo dnf install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -y
+
+# Add VS Code repo and install
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\n" | sudo tee /etc/yum.repos.d/vscode.repo
+dnf check-update
+sudo dnf install code -y
+
+# Install PowerShell Core
+curl https://packages.microsoft.com/config/rhel/7/prod.repo | sudo tee /etc/yum.repos.d/microsoft.repo
+dnf check-update
+sudo dnf install powershell -y
+
+# Add Insync repo and install
+sudo rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
+echo -e "[insync]\nname=insync repo\nbaseurl=http://yum.insync.io/fedora/30/\ngpgcheck=1\ngpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key\nenabled=1\nmetadata_expire=120m\n" | sudo tee /etc/yum.repos.d/insync.repo
+dnf check-update
+sudo dnf install insync -y
+
+# Install Viber
+sudo dnf install https://download.cdn.viber.com/desktop/Linux/viber.rpm -y
+
