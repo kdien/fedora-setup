a44d514e453ff62c701b992831d8dab0b563b56e
Khoa Dien <khoa.dien@hotmail.com>
2023-03-13 23:51:31 -0300

    Clean up config and update syntax for better readability

--- setup.sh
+++ setup.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
-# Setup bash symlinks
-[[ -f "$HOME"/.bashrc ]] && mv "$HOME"/.bashrc "$HOME"/.bashrc.bak
-ln -sf "$HOME"/fedora-setup/.bashrc "$HOME"/.bashrc
+# Source bash config
+cat >> "$HOME"/.bashrc <<'EOF'
+[[ -f "$HOME"/dotfiles/bash/.bash_common ]] && . "$HOME"/dotfiles/bash/.bash_common
+EOF
 
 # Clone dotfiles and setup symlinks
 git clone https://github.com/kdien/dotfiles.git "$HOME"/dotfiles
-ln -sf "$HOME"/dotfiles/shell/.profile "$HOME"/.profile
 ln -sf "$HOME"/dotfiles/tmux/.tmux.conf "$HOME"/.tmux.conf
 ln -sf "$HOME"/dotfiles/neovim "$HOME"/.config/nvim
+ln -sf "$HOME"/dotfiles/alacritty "$HOME"/.config/alacritty
 ln -sf "$HOME"/dotfiles/kitty "$HOME"/.config/kitty
-ln -sf "$HOME"/dotfiles/pureline/.pureline.conf "$HOME"/.pureline.conf
 ln -sf "$HOME"/dotfiles/powershell "$HOME"/.config/powershell
 
 # Configure GNOME settings
 if [[ "$XDG_CURRENT_DESKTOP" == *GNOME* ]]; then
-    sudo dnf install gnome-tweaks gnome-extensions-app gnome-shell-extension-appindicator -y
+    sudo dnf install -y gnome-tweaks gnome-extensions-app gnome-shell-extension-appindicator
     ./config-gnome.sh
     mkdir -p "$HOME"/bin
     for file in "$HOME"/dotfiles/gnome/*; do
@@ -32,18 +32,19 @@ rm -rf "$HOME"/.config/fontconfig
 ln -s "$HOME"/dotfiles/fontconfig "$HOME"/.config/fontconfig
 
 # Enable additional repos
-sudo dnf install "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm" -y
-dnf check-update
+sudo dnf install -y \
+    "https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm" \
+    "https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm"
 
 # Remove bloat
-sudo dnf remove "$(cat ./pkg.remove)" -y
+sudo dnf remove -y "$(cat ./pkg.remove)"
 
 # Install packages from repo
-sudo dnf install "$(cat ./pkg.add)" -y
+sudo dnf install -y "$(cat ./pkg.add)"
 
 # Set up interception-tools and caps2esc
-sudo dnf copr enable fszymanski/interception-tools -y
-sudo dnf install interception-tools -y
+sudo dnf copr enable -y fszymanski/interception-tools
+sudo dnf install -y interception-tools
 
 git clone https://gitlab.com/interception/linux/plugins/caps2esc.git
 cd caps2esc || return
@@ -54,14 +55,7 @@ cd ..
 rm -rf caps2esc
 
 sudo mkdir -p /etc/interception/udevmon.d
-sudo tee /etc/interception/udevmon.d/caps2esc.yaml <<'EOF'
-- JOB: intercept -g $DEVNODE | caps2esc -m 1 | uinput -d $DEVNODE
-  DEVICE:
-    EVENTS:
-      EV_KEY: [KEY_CAPSLOCK, KEY_ESC]
-    LINK: /dev/input/by-path/.*-event-kbd
-EOF
-
+sudo install -o root -g root -m 644 caps2esc.yaml /etc/interception/udevmon.d/caps2esc.yaml
 sudo systemctl enable --now udevmon
 
 # Install Firefox from Mozilla
@@ -78,24 +72,29 @@ echo MOZ_ENABLE_WAYLAND=1 | sudo tee -a /etc/environment
 # Install Brave browser
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser -y
+sudo dnf install -y brave-browser
 
 # Install MS Edge
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo dnf config-manager --add-repo https://packages.microsoft.com/yumrepos/edge
 sudo mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge.repo
-dnf check-update
-sudo dnf install microsoft-edge-stable -y
+sudo dnf install -y microsoft-edge-stable
 
 # Install Google Chrome
-sudo dnf install https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -y
+sudo dnf install -y https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
 # Override default desktop entry to enable dark mode
 sudo install -o root -g root -m 644 desktop-entries/google-chrome.desktop /usr/local/share/applications/google-chrome.desktop
 
 # Add VS Code repo and install
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc\n" | sudo tee /etc/yum.repos.d/vscode.repo
-dnf check-update
-sudo dnf install code -y
+sudo tee /etc/yum.repos.d/vscode.repo <<EOF
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+sudo dnf install -y code
 
 # Install tfenv and Terraform
 git clone --depth=1 https://github.com/tfutils/tfenv.git "$HOME"/.tfenv
@@ -104,11 +103,18 @@ git clone --depth=1 https://github.com/tfutils/tfenv.git "$HOME"/.tfenv
 
 # Add Insync repo and install
 sudo rpm --import https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
-echo -e "[insync]\nname=insync repo\nbaseurl=http://yum.insync.io/fedora/$(rpm -E %fedora)/\ngpgcheck=1\ngpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key\nenabled=1\nmetadata_expire=120m\n" | sudo tee /etc/yum.repos.d/insync.repo
-dnf check-update
-sudo dnf install insync -y
+sudo tee /etc/yum.repos.d/insync.repo <<EOF
+[insync]
+name=Insync
+baseurl=http://yum.insync.io/fedora/$(rpm -E %fedora)
+gpgcheck=1
+gpgkey=https://d2t3ff60b2tol4.cloudfront.net/repomd.xml.key
+enabled=1
+metadata_expire=120m
+EOF
+sudo dnf install -y insync
 if command -v nautilus &> /dev/null; then
-    sudo dnf install insync-nautilus -y
+    sudo dnf install -y insync-nautilus
 fi
 
 # Install Viber
@@ -122,17 +128,5 @@ sudo install -o root -g root -m 644 desktop-entries/viber.desktop /usr/share/app
 rm -f "$HOME"/Downloads/viber.AppImage
 
 # Install Zoom
-sudo dnf install https://zoom.us/client/latest/zoom_x86_64.rpm -y
-
-# Adobe Reader through Wine
-# export WINEARCH=win32
-# winetricks atmlib
-# winetricks riched20
-# winetricks wsh57
-# winetricks mspatcha
-# sudo mkdir -p /usr/share/fonts/segoe-ui
-# sudo cp "$HOME"/dotfiles/fonts/segoeui*.ttf /usr/share/fonts/segoe-ui/
-# curl -kL http://ardownload.adobe.com/pub/adobe/reader/win/AcrobatDC/1901020099/AcroRdrDC1901020099_en_US.exe -o "$HOME"/Downloads/adobereader.exe
-# wine "$HOME"/Downloads/adobereader.exe
-# echo 'After installing Adobe Reader, disable auto updates through Regedit, HKEY_LOCAL_MACHINE\Software\Adobe\Adobe ARM\Legacy\Reader\{key} and set Mode=0'
+sudo dnf install -y https://zoom.us/client/latest/zoom_x86_64.rpm
 
