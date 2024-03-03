9a8b39ead40fce018ac609c00861308b5563ca43
Khoa Dien <khoa.dien@hotmail.com>
2024-03-03 16:49:55 -0400

    Add script to replace GNOME with KDE Plasma

---
+++ gnome-to-kde.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+sudo dnf shell -y --setopt protected_packages= << EOI
+swap fedora-release-workstation fedora-release-kde
+swap fedora-release-identity-workstation fedora-release-identity-kde
+run
+remove @gnome-desktop
+run
+remove *gnome* *gtk*
+run
+install @kde-desktop-environment
+run
+EOI
+sudo systemctl restart sddm.service
+EOF
